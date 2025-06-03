fbe235a23e9b2d832f23a5f3c0def91f7790f440
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-03 15:27:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import "./App.css";
 
 function App() {
   const handleOpen = ()=>{
-       window.location.href=  'https://otp-frontend-seven.vercel.app/MainApp'
+       window.location.href=  'maadhaar://'
+    // Fallback after delay if app didn't open
+      setTimeout(() => {
+        if (Date.now() - now < 5000) {
+        
+          window.location.href = "https://apps.apple.com/app/id1435469474";        }
+      }, 4500);
   }
 
   return (
