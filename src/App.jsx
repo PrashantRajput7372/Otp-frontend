68ea72f96770afe35fc38e7662759ca8abcd42a5
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-03 16:09:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 function App() {
   const handleOpen = () => {
-    const appScheme = "maadhaar://";
-    const appStoreURL = "https://apps.apple.com/app/id1435469474";
+    window.location.href = 'https://otp-frontend-seven.vercel.app/MainApp';
 
-    const timeout = setTimeout(() => {
-      window.location.href = appStoreURL;
-    }, 2000);
-
-    // Listen for page becoming hidden (app opened)
-    const handleVisibilityChange = () => {
-      if (document.hidden) {
-        clearTimeout(timeout); // App opened, cancel fallback
-      }
-    };
-
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-
-    // Attempt to open app
-    window.location.href = appScheme;
-
-    // Cleanup after 3 seconds
-    setTimeout(() => {
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    }, 3000);
   };
 
   return (
