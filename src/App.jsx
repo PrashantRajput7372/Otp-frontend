f925022bd015852c361f86b6a0f38626f8f6267a
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-03 15:34:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import "./App.css";
 function App() {
   const handleOpen = ()=>{
        window.location.href=  'maadhaar://'
+    const now = Date.now();
     // Fallback after delay if app didn't open
       setTimeout(() => {
-        if (Date.now() - now < 5000) {
+        if (Date.now() - now < 3000) {
         
           window.location.href = "https://apps.apple.com/app/id1435469474";        }
-      }, 4500);
+      }, 2000);
   }
 
   return (
