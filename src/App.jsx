c7148de1d35999a3a29bddb0e38430d35c462b82
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-03 17:05:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={handleOpen}>Open App JAI MATA DI</button>
+      <button onClick={handleOpen}>Open App MahaDEV</button>
     </div>
   );
 }
