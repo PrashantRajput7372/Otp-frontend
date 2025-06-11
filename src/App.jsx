8e89e92b736397937419b360d59dfd4d6a9dc9c5
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-11 14:33:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 function App() {
   const handleOpen = () => {
-    window.location.href = "maadhaar://";
+    window.location.href = "https://otp-frontend-seven.vercel.app/mypath";
   };
-  function showSmartAppBanner() {
-    const meta = document.createElement("meta");
-    meta.name = "apple-itunes-app";
-    meta.content = "app-id=1435469474";
-    document.head.appendChild(meta);
-
-    // iOS Safari loads the banner only if the meta tag is present at page load.
-    // This trick might not work unless you reload the page.
-    // So you may need to trigger a reload:
-    setTimeout(() => {
-      window.location.reload();
-    }, 100); // Short delay to allow DOM to update
-  }
+  
   return (
     <div className="App">
-      <button onClick={showSmartAppBanner}>Open App MahaDEV 3</button>
+      <button onClick={handleOpen}>Open App MahaDEV 2</button>
     </div>
   );
 }
