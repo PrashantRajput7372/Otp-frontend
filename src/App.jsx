9bda663c6d774c70e17e19ed06ec954770efcd84
Prashant Singh <prashantsingh113113@gmail.com>
2025-06-17 16:28:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import HomePage from './HomePage'
 
 function App() {
-  const handleOpen = () => {
-    location.href = "https://try-ecru-two.vercel.app/extra-path-1/ulink";
-  };
-  
+  const handleOpenApp = ()=>{
+  const universalLink = “https://try-ecru-two.vercel.app/extra-path-1/ulink"
+  window.location.href = universalLink
+}  
   return (
     <div className="App">
-     <a href="https://try-ecru-two.vercel.app/extra-path-1/ulink">Open mAaadhar</a>
-      <div onclick= "location.href='https://try-ecru-two.vercel.app/extra-path-1/ulink';">open App</div>
+     <a href="https://try-ecru-two.vercel.app/extra-path-1/ulink" style={{
+                width: "40%",
+                padding: "8px 16px",
+                backgroundColor: "#1976d2",
+                color: "#fff",
+                borderRadius: "4px",
+                textAlign: "center",
+                fontWeight: "bold",
+                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}>Open mAaadhar with anchor</a>
+      <Button
+            variant="contained"
+            color="primary"
+            style={{ width: "40%" }}
+            onClick={handleOpenApp}
+          >
+            Open by Button CLick
+          </Button>
      
     </div>
   );
